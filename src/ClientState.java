65f945148f887e9976ad9a31ecca98504cc608f7
Simão Silva <37107350+SFOS-SFOS@users.noreply.github.com>
2018-11-21 20:26:24 +0000

    Custom errors

--- src/ClientState.java
+++ src/ClientState.java
@@ -1,44 +1,44 @@
 public class ClientState {
-  private String room;
-  private String nick;
-  private String state;
-  
-  
-  public ClientState(String nick) {
-    room = "";
-    this.nick = nick;
-    state = "outside";
-  }
-  
-  public ClientState() {
-    room = "";
-    this.nick = "";
-    state = "init";
-  }
-  
-  
-  public void setNick(String nick) {
-    this.nick = nick;
-  }
-  
-  public String getNick() {
-    return this.nick;
-  }
-  
-  public void setRoom(String roomName) {
-    this.room = roomName;
-  }
-  
-  public String getRoom() {
-    return this.room;
-  }
-  
-  void setState(String state) {
-    this.state = state;
-  }
-  
-  public String getState() {
-    return this.state;
-  }
-  
+	private String room;
+	private String nick;
+	private String state;
+
+
+	public ClientState(String nick) {
+		room = "";
+		this.nick = nick;
+		state = "outside";
+	}
+
+	public ClientState() {
+		room = "";
+		this.nick = "";
+		state = "init";
+	}
+
+
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+
+	public String getNick() {
+		return this.nick;
+	}
+
+	public void setRoom(String roomName) {
+		this.room = roomName;
+	}
+
+	public String getRoom() {
+		return this.room;
+	}
+
+	void setState(String state) {
+		this.state = state;
+	}
+
+	public String getState() {
+		return this.state;
+	}
+
 }
--- src/Responses.java
+++ src/Responses.java
@@ -9,77 +9,78 @@ import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
 
 class Responses {
-  
-  static private final Charset charset = Charset.forName("UTF8");
-  static private final CharsetEncoder encoder = charset.newEncoder();
-  
-  static void acceptedNickResponse(SelectionKey key, String oldNick, String newNick, Selector selector) {
-    //TODO SUCCESS, pode utilizar esse nick
-    sendMessageToClient(key, "OK");
-    if (((ClientState) key.attachment()).getState().compareTo("inside") == 0) {
-      diffuseToChatRoom(key, ((ClientState) key.attachment()).getRoom(), "NEWNICK " + oldNick + " " + newNick, selector, false);
+
+	static private final Charset charset = Charset.forName("UTF8");
+	static private final CharsetEncoder encoder = charset.newEncoder();
+
+	static void acceptedNickResponse(SelectionKey key, String oldNick, String newNick, Selector selector) {
+		//TODO SUCCESS, pode utilizar esse nick
+		sendMessageToClient(key, "OK");
+		if (((ClientState) key.attachment()).getState().compareTo("inside") == 0) {
+			diffuseToChatRoom(key, ((ClientState) key.attachment()).getRoom(), "NEWNICK " + oldNick + " " + newNick, selector, false);
 //            diffuseToChatRoom(key, ((ClientState) key.attachment()).getRoom(), oldNick + " mudou de nome " + newNick, selector, false);
-    }
-  }
-  
-  static void sendErrorResponse(SelectionKey key) {
-    //TODO ERROR, nome já escolhido
-    sendMessageToClient(key, "ERROR");
-  }
-  
-  static void joinedRoomResponse(SelectionKey key, String nickname, Selector selector) {
-    sendMessageToClient(key, "OK");
-    diffuseToChatRoom(key, ((ClientState) key.attachment()).getRoom(), "JOINED " + nickname, selector, false);
-  }
-  
-  
-  static void leaveRoomResponseToClient(SelectionKey key) {
-    sendMessageToClient(key, "OK");
-  }
-  
-  static void leaveRoomResponseToOthers(SelectionKey key, String whoLeft, Selector selector) {
-    diffuseToChatRoom(key, ((ClientState) key.attachment()).getRoom(), "LEFT " + whoLeft, selector, false);
-  }
-  
-  static void byeResponse(SelectionKey key) {
-    sendMessageToClient(key, "BYE");
-  }
-  
-  static void diffuseToChatRoom(SelectionKey k, String room, String message, Selector selector, boolean sendToOwner) {
-    for (SelectionKey key : selector.keys()) {
-      if (!key.isAcceptable() && (sendToOwner || !key.equals(k))) {
-        if (key.attachment() != null) {
-          if (((ClientState) key.attachment()).getRoom().compareTo(room) == 0 && ((ClientState) key.attachment()).getState().compareTo("inside") == 0) {
-            sendMessageToClient(key, message);
-          }
-        }
-      }
-    }
-  }
-  
-  static void sendPrivateMessageToClient(SelectionKey senderKey, SelectionKey receiverKey, String message) {
-    String sender = ((ClientState) senderKey.attachment()).getNick();
-    sendMessageToClient(receiverKey, "PRIVATE " + sender + " " + message);
-    sendMessageToClient(senderKey, "OK");
-  }
-  
-  private static void sendMessageToClient(SelectionKey key, String message) {
-    SocketChannel sc = (SocketChannel) key.channel();
+		}
+	}
+
+	static void sendErrorResponse(SelectionKey key) {
+		//TODO ERROR, nome já escolhido
+		sendMessageToClient(key, "ERROR");
+	}
+
+	static void joinedRoomResponse(SelectionKey key, String nickname, Selector selector) {
+		sendMessageToClient(key, "OK");
+		diffuseToChatRoom(key, ((ClientState) key.attachment()).getRoom(), "JOINED " + nickname, selector, false);
+	}
+
+
+	static void leaveRoomResponseToClient(SelectionKey key) {
+		sendMessageToClient(key, "OK");
+	}
+
+	static void leaveRoomResponseToOthers(SelectionKey key, String whoLeft, Selector selector) {
+		diffuseToChatRoom(key, ((ClientState) key.attachment()).getRoom(), "LEFT " + whoLeft, selector, false);
+	}
+
+	static void byeResponse(SelectionKey key) {
+		sendMessageToClient(key, "BYE");
+	}
+
+	static void diffuseToChatRoom(SelectionKey k, String room, String message, Selector selector, boolean sendToOwner) {
+		for (SelectionKey key : selector.keys()) {
+			if (!key.isAcceptable() && (sendToOwner || !key.equals(k))) {
+				if (key.attachment() != null) {
+					if (((ClientState) key.attachment()).getRoom().compareTo(room) == 0 && ((ClientState) key.attachment()).getState().compareTo("inside") == 0) {
+						sendMessageToClient(key, message);
+					}
+				}
+			}
+		}
+	}
+
+	static void sendPrivateMessageToClient(SelectionKey senderKey, SelectionKey receiverKey, String message) {
+		String sender = ((ClientState) senderKey.attachment()).getNick();
+		sendMessageToClient(receiverKey, "PRIVATE " + sender + " " + message);
+		sendMessageToClient(senderKey, "OK");
+	}
+
+	private static void sendMessageToClient(SelectionKey key, String message) {
+		SocketChannel sc = (SocketChannel) key.channel();
 //        Socket s = sc.socket();
-    ByteBuffer buffer = ByteBuffer.allocate(16384);
-    buffer.clear();
-    try {
-      buffer.put(encoder.encode(CharBuffer.wrap(message + "\n")));
-      buffer.flip();
-      try {
-        sc.write(buffer);
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-      buffer.rewind();
-    } catch (CharacterCodingException e) {
-      e.printStackTrace();
-    }
-  }
+		ByteBuffer buffer = ByteBuffer.allocate(16384);
+		buffer.clear();
+		try {
+			buffer.put(encoder.encode(CharBuffer.wrap(message + "\n")));
+			buffer.flip();
+			try {
+				sc.write(buffer);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			buffer.rewind();
+		} catch (CharacterCodingException e) {
+			e.printStackTrace();
+		}
+	}
 }
+
 
